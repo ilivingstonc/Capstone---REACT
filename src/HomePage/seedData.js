61e1a8dc7ef753f3945ce68e4c7dd9367e306076
Ian Livingston <i.livingston.c@gmail.com>
2019-12-04 17:00:47 -0700

    Unnest react repo

---
+++ src/HomePage/seedData.js
@@ -0,0 +1,285 @@
+
+const seedData = [
+    {
+        "id": 763,
+        "resource_state": 2,
+        "name": "Kerr Gulch",
+        "activity_type": "Ride",
+        "distance": 6776.05,
+        "average_grade": 4.4,
+        "maximum_grade": 12.2,
+        "elevation_high": 2378.8,
+        "elevation_low": 2081.9,
+        "start_latlng": [
+            39.660513,
+            -105.291082
+        ],
+        "end_latlng": [
+            39.693321,
+            -105.33811
+        ],
+        "start_latitude": 39.660513,
+        "start_longitude": -105.291082,
+        "end_latitude": 39.693321,
+        "end_longitude": -105.33811,
+        "climb_category": 2,
+        "city": "Evergreen",
+        "state": "CO",
+        "country": "United States",
+        "private": false,
+        "hazardous": false,
+        "starred": true,
+        "pr_time": 1504,
+        "athlete_pr_effort": {
+            "id": 53197854418,
+            "elapsed_time": 1504,
+            "distance": 6775.4,
+            "start_date": "2018-05-28T01:22:06Z",
+            "start_date_local": "2018-05-27T19:22:06Z",
+            "is_kom": false
+        },
+        "starred_date": "2018-03-12T01:25:44Z"
+    },
+    {
+        "id": 609371,
+        "resource_state": 2,
+        "name": "Lookout Mountain (Gate-BB Grave)",
+        "activity_type": "Ride",
+        "distance": 7361.19,
+        "average_grade": 5.4,
+        "maximum_grade": 31.1,
+        "elevation_high": 2230.3,
+        "elevation_low": 1843.0,
+        "start_latlng": [
+            39.740329,
+            -105.227683
+        ],
+        "end_latlng": [
+            39.731674,
+            -105.239256
+        ],
+        "start_latitude": 39.740329,
+        "start_longitude": -105.227683,
+        "end_latitude": 39.731674,
+        "end_longitude": -105.239256,
+        "climb_category": 3,
+        "city": "Golden",
+        "state": "Colorado",
+        "country": "United States",
+        "private": false,
+        "hazardous": false,
+        "starred": true,
+        "pr_time": 1189,
+        "athlete_pr_effort": {
+            "id": 61355742708,
+            "elapsed_time": 1189,
+            "distance": 7273.2,
+            "start_date": "2019-06-08T16:40:12Z",
+            "start_date_local": "2019-06-08T10:40:12Z",
+            "is_kom": false
+        },
+        "starred_date": "2018-02-05T04:24:21Z"
+    },
+    {
+        "id": 642991,
+        "resource_state": 2,
+        "name": "Mt Evans HC",
+        "activity_type": "Ride",
+        "distance": 44325.8,
+        "average_grade": 4.5,
+        "maximum_grade": 9.6,
+        "elevation_high": 4252.2,
+        "elevation_low": 2268.0,
+        "start_latlng": [
+            39.738750122487545,
+            -105.52325952798128
+        ],
+        "end_latlng": [
+            39.58746129646897,
+            -105.64365810714662
+        ],
+        "start_latitude": 39.738750122487545,
+        "start_longitude": -105.52325952798128,
+        "end_latitude": 39.58746129646897,
+        "end_longitude": -105.64365810714662,
+        "climb_category": 5,
+        "city": "Idaho Springs",
+        "state": "CO",
+        "country": "United States",
+        "private": false,
+        "hazardous": false,
+        "starred": true,
+        "pr_time": 7989,
+        "athlete_pr_effort": {
+            "id": 64481275422,
+            "elapsed_time": 7989,
+            "distance": 43547.5,
+            "start_date": "2019-07-27T13:35:01Z",
+            "start_date_local": "2019-07-27T07:35:01Z",
+            "is_kom": false
+        },
+        "starred_date": "2018-03-12T01:19:34Z"
+    },
+    {
+        "id": 1747567,
+        "resource_state": 2,
+        "name": "2012 Pikes Peak Hill Climb",
+        "activity_type": "Ride",
+        "distance": 19471.3,
+        "average_grade": 7.3,
+        "maximum_grade": 12.8,
+        "elevation_high": 4301.8,
+        "elevation_low": 2872.8,
+        "start_latlng": [
+            38.920994298532605,
+            -105.03750217147171
+        ],
+        "end_latlng": [
+            38.84006107226014,
+            -105.0433087348938
+        ],
+        "start_latitude": 38.920994298532605,
+        "start_longitude": -105.03750217147171,
+        "end_latitude": 38.84006107226014,
+        "end_longitude": -105.0433087348938,
+        "climb_category": 5,
+        "city": "Woodland Park",
+        "state": "CO",
+        "country": "United States",
+        "private": false,
+        "hazardous": false,
+        "starred": true,
+        "pr_time": 5572,
+        "athlete_pr_effort": {
+            "id": 44258956722,
+            "elapsed_time": 5572,
+            "distance": 19591.3,
+            "start_date": "2018-08-11T12:40:04Z",
+            "start_date_local": "2018-08-11T06:40:04Z",
+            "is_kom": false
+        },
+        "starred_date": "2018-07-20T03:23:03Z"
+    },
+    {
+        "id": 4993829,
+        "resource_state": 2,
+        "name": "Super Duper Lookout (all the way to top of Mt. Vernon)",
+        "activity_type": "Ride",
+        "distance": 16114.5,
+        "average_grade": 3.6,
+        "maximum_grade": 39.8,
+        "elevation_high": 2388.7,
+        "elevation_low": 1792.5,
+        "start_latlng": [
+            39.745278,
+            -105.224226
+        ],
+        "end_latlng": [
+            39.716877,
+            -105.294308
+        ],
+        "start_latitude": 39.745278,
+        "start_longitude": -105.224226,
+        "end_latitude": 39.716877,
+        "end_longitude": -105.294308,
+        "climb_category": 3,
+        "city": "Golden",
+        "state": "CO",
+        "country": "United States",
+        "private": false,
+        "hazardous": false,
+        "starred": true,
+        "pr_time": 2532,
+        "athlete_pr_effort": {
+            "id": 61510209973,
+            "elapsed_time": 2532,
+            "distance": 16037.4,
+            "start_date": "2019-06-10T17:14:57Z",
+            "start_date_local": "2019-06-10T11:14:57Z",
+            "is_kom": false
+        },
+        "starred_date": "2019-02-05T20:07:33Z"
+    },
+    {
+        "id": 6692073,
+        "resource_state": 2,
+        "name": "Golden Gate Loop",
+        "activity_type": "Ride",
+        "distance": 32114.6,
+        "average_grade": 0.1,
+        "maximum_grade": 29.1,
+        "elevation_high": 2659.0,
+        "elevation_low": 2096.8,
+        "start_latlng": [
+            39.778854,
+            -105.286815
+        ],
+        "end_latlng": [
+            39.77871,
+            -105.286697
+        ],
+        "start_latitude": 39.778854,
+        "start_longitude": -105.286815,
+        "end_latitude": 39.77871,
+        "end_longitude": -105.286697,
+        "climb_category": 0,
+        "city": "Golden",
+        "state": "CO",
+        "country": "United States",
+        "private": false,
+        "hazardous": false,
+        "starred": true,
+        "pr_time": 4792,
+        "athlete_pr_effort": {
+            "id": 53197833414,
+            "elapsed_time": 4792,
+            "distance": 32043.9,
+            "start_date": "2018-07-11T00:42:35Z",
+            "start_date_local": "2018-07-10T18:42:35Z",
+            "is_kom": false
+        },
+        "starred_date": "2018-03-12T00:57:29Z"
+    },
+    {
+        "id": 20884908,
+        "resource_state": 2,
+        "name": "LOM - Mt.Vern - Genesee Mt.",
+        "activity_type": "Ride",
+        "distance": 18514.6,
+        "average_grade": 3.1,
+        "maximum_grade": 15.4,
+        "elevation_high": 2408.6,
+        "elevation_low": 1826.6,
+        "start_latlng": [
+            39.740506,
+            -105.227839
+        ],
+        "end_latlng": [
+            39.697056,
+            -105.289334
+        ],
+        "start_latitude": 39.740506,
+        "start_longitude": -105.227839,
+        "end_latitude": 39.697056,
+        "end_longitude": -105.289334,
+        "climb_category": 3,
+        "city": null,
+        "state": null,
+        "country": null,
+        "private": false,
+        "hazardous": false,
+        "starred": true,
+        "pr_time": 2981,
+        "athlete_pr_effort": {
+            "id": 61799330694,
+            "elapsed_time": 2981,
+            "distance": 18514.6,
+            "start_date": "2019-06-15T15:20:15Z",
+            "start_date_local": "2019-06-15T09:20:15Z",
+            "is_kom": false
+        },
+        "starred_date": "2019-08-04T13:49:56Z"
+    }
+]
+
+module.exports = seedData;
